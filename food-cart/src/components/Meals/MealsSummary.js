b7648d6ff31ea90ff5f12ed54c990bf65d4c0335
alamin-7 <alamin188914@gmail.com>
2023-02-28 12:18:07 +0600

    components added

---
+++ food-cart/src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Free for you</h2>
+      <p>
+        Stay connecte with this app to be notified
+      </p>
+      <p>
+        You will get the free meals with delicious taste!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
